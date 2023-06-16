4f80ba86d968ecb681e8b2a590ca9b0b7756722c
mayurkd36 <105969274+Mayurkd36@users.noreply.github.com>
2023-06-16 18:04:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import UserRegistrationForm from './components/LoginPage/UserRegistrationForm.js';
+import './components/LoginPage/UserRegistrationForm.css';
+import Login from './components/LoginPage/Login.js';
+
+import RestPassword from './components/LoginPage/RestPassword.js';
+import AddData from './components/SignInPageData/AddData.js';
+import AllTicket from './components/SignInPageData/AllTicket.js';
+
+import AllMovie from './components/SignInPageData/AllMovie.js';
+import AddMovieForm from './components/SignInPageData/AddMovieForm.js';
+import DeleteMovie from './components/SignInPageData/DeleteMovie.js';
+import User from './components/SignInPageData/User.js';
+import AddTicket from './components/SignInPageData/AddTicket.js';
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/UserRegistrationForm" element={<UserRegistrationForm />} />
+        <Route path="/RestPassword" element={<RestPassword />} />
+        <Route path="/AddData" element={<AddData />} />
+        <Route path="/AllTicket" element={<AllTicket />} />
+        <Route path="/AllMovie" element={<AllMovie />} />
+        <Route path="/AddMovieForm" element={<AddMovieForm />} />
+        <Route path="/AddData" element={<AddData />} />
+        <Route path="/DeleteMovie" element={<DeleteMovie />} />
+        <Route path="/User" element={<User />} />
+
+        <Route path="/AddTicket" element={<AddTicket />} />
+        
+      </Routes>
+    </Router>
+   
     </div>
   );
 }
